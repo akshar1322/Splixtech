a66bc3e64c3cf7ef889a7569e01558d7d4a14fba
Akshar <113759354+akshar1322@users.noreply.github.com>
2024-08-02 04:12:42 +0530

    chore: Update landing page banner text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         spli<span className="text-purple-400">X</span>tech
       </h1>
       <p className="font-mono text-lg sm:text-xl md:text-2xl lg:text-3xl z-10 text-center mt-4 sm:mt-6 md:mt-8">
-        Akshar Patel presents iDevNow Tech, transforming the digital landscape with cutting-edge web development, AI, ML, advanced software solutions, <span className="text-green-400">exceptional UI/UX design, and 3D product design,</span> in partnership with Redmagic Studio.
+        Akshar Patel presents  SpliXTech, transforming the digital landscape with cutting-edge web development, AI, ML, advanced software solutions, <span className="text-green-400">exceptional UI/UX design, and 3D product design,</span> in partnership with Redmagic Studio.
       </p>
       <button
         className="z-10 mt-4 sm:mt-6 md:mt-8 px-4 sm:px-6 py-2 sm:py-3 bg-green-500 text-white rounded-lg text-base sm:text-lg md:text-xl font-bold hover:bg-green-600 transition duration-300"
